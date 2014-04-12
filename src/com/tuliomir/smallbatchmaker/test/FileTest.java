ffbe6c461a2d1618caf4ce6f10c26709f8911f90
tuliomir <tulio.mir@gmail.com>
2014-04-11 23:39:34 -0300

    Refactored physical file manipulation

--- src/com/tuliomir/smallbatchmaker/test/FileTest.java
+++ src/com/tuliomir/smallbatchmaker/test/FileTest.java
@@ -3,10 +3,8 @@
  */
 package com.tuliomir.smallbatchmaker.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Random;
 
 import org.junit.Test;
@@ -21,65 +19,30 @@ public class FileTest {
 	
 	private static final String EXAMPLE_TXT_FILENAME = "example.txt";
 
-	private java.io.File createTestFile(String fileName) {
-		java.io.File physicalFile = new java.io.File(fileName);
-		
-		try {
-			if (!physicalFile.createNewFile()) {
-				physicalFile.delete();
-				if (!physicalFile.createNewFile()) {
-					throw new Exception("Cannot empty file.");
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return physicalFile;
-	}
 	
-	private void incrementPhysicalFile(java.io.File physicalFile, String incrementalString) {
-		try {
-			FileWriter out = new FileWriter(physicalFile);
-			out.append(incrementalString);
-			out.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private void destroyTestFile(String fileName) {
-		java.io.File physicalFile = new java.io.File(fileName);
-		physicalFile.delete();
-	}
 
 	@Test
 	public void shouldFindFile() {
-		File testFile = new File(createTestFile(EXAMPLE_TXT_FILENAME));
+		File testFile = new File(PhysicalFile.createTestFile(EXAMPLE_TXT_FILENAME));
 		assertEquals(EXAMPLE_TXT_FILENAME,testFile.getFileName());
-		destroyTestFile(EXAMPLE_TXT_FILENAME);
+		PhysicalFile.destroyTestFile(EXAMPLE_TXT_FILENAME);
 	}
 	
 	@Test
 	public void shouldReturnZeroSize() {
-		File testFile = new File(createTestFile(EXAMPLE_TXT_FILENAME));
+		File testFile = new File(PhysicalFile.createTestFile(EXAMPLE_TXT_FILENAME));
 		assertEquals(0l,testFile.getFileSize());
-		destroyTestFile(EXAMPLE_TXT_FILENAME);
+		PhysicalFile.destroyTestFile(EXAMPLE_TXT_FILENAME);
 	}
 	
 	@Test
 	public void shouldReturnSizeOfOne() {
-		java.io.File physicalFile = createTestFile(EXAMPLE_TXT_FILENAME);
-		incrementPhysicalFile(physicalFile, "a");
+		java.io.File physicalFile = PhysicalFile.createTestFile(EXAMPLE_TXT_FILENAME,"a");
 		File testFile = new File(physicalFile);
 		
 		assertEquals(1l,testFile.getFileSize());
 		
-		destroyTestFile(EXAMPLE_TXT_FILENAME);
+		PhysicalFile.destroyTestFile(EXAMPLE_TXT_FILENAME);
 	}
 	
 	@Test
@@ -98,13 +61,12 @@ public class FileTest {
 		testString = outputBuffer.toString();
 		
 		// Pushing the string into the file
-		java.io.File physicalFile = createTestFile(EXAMPLE_TXT_FILENAME);
-		incrementPhysicalFile(physicalFile, testString);
+		java.io.File physicalFile = PhysicalFile.createTestFile(EXAMPLE_TXT_FILENAME, testString);
 		File testFile = new File(physicalFile);
 		
 		assertEquals(testString.length(),testFile.getFileSize());
 		
-		destroyTestFile(EXAMPLE_TXT_FILENAME);
+		PhysicalFile.destroyTestFile(EXAMPLE_TXT_FILENAME);
 	}
 
 }
---
+++ src/com/tuliomir/smallbatchmaker/test/PhysicalFile.java
@@ -0,0 +1,61 @@
+package com.tuliomir.smallbatchmaker.test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PhysicalFile {
+	
+	/**
+	 * Creates an empty test file
+	 * @param fileName Name of the file to be created
+	 * @return java.io.File pointing to the empty file
+	 */
+	public static java.io.File createTestFile(String fileName) {
+		java.io.File physicalFile = new java.io.File(fileName);
+		
+		try {
+			if (!physicalFile.createNewFile()) {
+				physicalFile.delete();
+				if (!physicalFile.createNewFile()) {
+					throw new Exception("Cannot empty file.");
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return physicalFile;
+	}
+	
+	
+	/**
+	 * Creates a test file with a specified content
+	 * @param fileName Name of the file to be created
+	 * @param fileContent Content of the file
+	 * @return java.io.File containing the specified content
+	 */
+	public static java.io.File createTestFile(String fileName, String fileContent) {
+		java.io.File physicalFile = createTestFile(fileName);
+		incrementPhysicalFile(physicalFile, fileContent);
+		return physicalFile;
+	}
+	
+	private static void incrementPhysicalFile(java.io.File physicalFile, String incrementalString) {
+		try {
+			FileWriter out = new FileWriter(physicalFile);
+			out.append(incrementalString);
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void destroyTestFile(String fileName) {
+		java.io.File physicalFile = new java.io.File(fileName);
+		physicalFile.delete();
+	}
+}
